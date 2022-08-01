3c1a7518225fceb8ac5bc5882ac5b8b0473d6298
Jan Palimąka <25018671+tatry@users.noreply.github.com>
2022-08-01 12:20:38 +0200

    Create default network

--- test.sh
+++ test.sh
@@ -4,22 +4,22 @@
 
 virsh net-list --all
 
-# IP=`virsh net-dhcp-leases default  | grep inner | awk '{print $5}' | awk -F '/' '{print $1}'`
+IP=`virsh net-dhcp-leases default  | grep inner | awk '{print $5}' | awk -F '/' '{print $1}'`
 
-# wait-for-it "$IP:22" -t 300 -- echo ready
+wait-for-it "$IP:22" -t 300 -- echo ready
 
-# sshpass -p ubuntu ssh -o "StrictHostKeyChecking=no" "ubuntu@$IP" uname -a
+sshpass -p ubuntu ssh -o "StrictHostKeyChecking=no" "ubuntu@$IP" uname -a
 
-# echo ""
+echo ""
 
-# sshpass -p ubuntu ssh "ubuntu@$IP" pwd
+sshpass -p ubuntu ssh "ubuntu@$IP" pwd
 
-# echo ""
+echo ""
 
-# sshpass -p ubuntu ssh "ubuntu@$IP" df -h
+sshpass -p ubuntu ssh "ubuntu@$IP" df -h
 
-# virsh shutdown inner
+virsh shutdown inner
 
-# # success
+# success
 exit 0
 
