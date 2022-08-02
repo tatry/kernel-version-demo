e62d8276c6046f16a157c6b4b81bc002c1c26605
Jan Palimąka <25018671+tatry@users.noreply.github.com>
2022-08-02 10:40:17 +0200

    Boot kernel directly

--- install_vm.sh
+++ install_vm.sh
@@ -7,9 +7,11 @@ OS_TYPE="ubuntu-20.04"
 VM_NAME="inner"
 DISK_IMAGE="./$VM_NAME.qcow2"
 
-USER_NAME="ubuntu"
-USER_PASS=`mkpasswd ubuntu`
-USER_HOME=`pwd`
+mkdir inner
+guestmount -a inner.qcow2 -i --ro inner/
+sudo cp inner/boot/initrd.img-5.8.0-63-generic .
+sudo cp inner/boot/vmlinuz-5.8.0-63-generic .
+sudo guestunmount inner
 
 virt-install --import \
     --name "$VM_NAME" \
@@ -20,4 +22,5 @@ virt-install --import \
     --network network:default \
     --graphics none \
     --console pty,target_type=serial \
-    --noautoconsole
+    --noautoconsole \
+    --boot kernel=./vmlinuz-5.8.0-63-generic,initrd=./initrd.img-5.8.0-63-generic,kernel_args="ro console=tty0 console=ttyS0,115200n8 root=/dev/vda5"
