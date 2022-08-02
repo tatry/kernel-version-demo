479e8cb0e5df23e421f9403b95b841cf167228f0
Jan Palimąka <25018671+tatry@users.noreply.github.com>
2022-08-02 14:29:22 +0200

    Wait for guest on shutdown

--- test.sh
+++ test.sh
@@ -10,8 +10,10 @@ set -x
 
 sshpass -p ubuntu ssh -o "StrictHostKeyChecking=no" "ubuntu@$IP" uname -a
 
-sshpass -p ubuntu ssh "ubuntu@$IP" pwd
+sshpass -p ubuntu ssh "ubuntu@$IP" "pwd; ls"
 
 sshpass -p ubuntu ssh "ubuntu@$IP" df -h
 
 sudo virsh shutdown inner
+# wait for guest to shutdown itself
+sudo virsh console inner > /dev/null
